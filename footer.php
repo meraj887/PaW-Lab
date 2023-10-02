b172abaac3d0c3e2b3e721868de4f580685c3306
meraj887 <141300804+meraj887@users.noreply.github.com>
2023-10-02 11:16:56 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,13 @@
+<!-- Footer -->
+<footer class="d-flex flex-wrap justify-content-center align-items-center text-white-50 bg-dark fixed-bottom">
+
+    <!-- <p class="col-md-4 mb-0 text-muted px-5"><a href="https://apm.iitm.ac.in/" target="_blank">AMBE</a></p> -->
+
+    <p class="mb-0"> Designed by <a href="https://www.linkedin.com/in/meraj87" target="_blank" class="meraj">Meraj</a></p>
+
+    <!-- <p class="nav col-md-4 justify-content-end px-5"> <a href="https://www.iitm.ac.in/" target="_blank">IIT Madras</a> </p> -->
+
+</footer>
+<!-- Footer -->
+
+<!-- for all three, replace "justify-content-center" with "justify-content-between" -->
---
+++ header.php
@@ -0,0 +1,52 @@
+<!-- Header -->
+<div class="container-fluid d-none d-lg-block px-0">
+    <img src="Materials/Home/Header_Photo.jpg" class="figure-img img-fluid my-0" alt="" />
+
+    <div class="lab_name">
+        <h1>Particles and Waves Lab</h1>
+    </div>
+
+</div>
+
+
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark py-0">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">HOME</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="https://www.iitm.ac.in/" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        PEOPLE
+                    </a>
+                    <ul class="nav-item dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li><a class="nav-item dropdown-item" href="group.php">Group</a></li>
+                        <li><a class="nav-item dropdown-item" href="alumni.php">Alumni</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="research.php">RESEARCH</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="teaching.php">TEACHING</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.php">CONTACT</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="gallery.php">GALLERY</a>
+                </li>
+
+
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- Navbar -->
+<!-- Header -->
